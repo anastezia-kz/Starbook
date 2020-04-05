18f5536e1b3b01ab5986045cb30a95684feef8a9
teo23 <matteoazzola23@gmail.com>
2020-04-05 17:33:30 +0200

    added photo upload not working yet

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose')
 const User = require("../models/user");
 const axios = require("axios")
+// photo upload 
+const Photo = require('../models/photo.js');
+const uploadCloud = require('../config/cloudinary.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -25,5 +28,38 @@ router.get('/profile/:id', (req, res, next) => {
         });
     })
 })
+
+// photo upload
+
+router.get('/', (req, res, next) => {
+  Photo.find()
+  .then((photos) => {
+    res.render('index', { photos });
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});
+
+router.get('/photo/add', (req, res, next) => {
+  res.render('photo-add');
+});
+
+
+router.post('/photo/add', uploadCloud.single('photo'), (req, res, next) => {
+  // const { title, description } = req.body;
+  const imgPath = req.file.url;
+  const imgName = req.file.originalname;
+  const newPhoto = new Photo({imgPath, imgName})
+  // const newPhoto = new Photo({title, description, imgPath, imgName})
+  newPhoto.save()
+  .then(photo => {
+    res.redirect('/');
+  })
+  .catch(error => {
+    console.log(error);
+  })
+});
+
 
 module.exports = router;
